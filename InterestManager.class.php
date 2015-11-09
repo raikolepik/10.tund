27ebd302786431296f0a52b5b95b1a6c1b5e3e58
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-11-09 14:24:43 +0200

    lisamine

---
+++ InterestManager.class.php
@@ -0,0 +1,91 @@
+<?php
+class InterestManager {
+    
+    private $connection;
+    private $user_id;
+    
+    function __construct($mysqli, $user_id){
+        
+        $this->connection = $mysqli;
+        $this->user_id = $user_id;
+        
+    }
+    
+    function addInterest($name){
+        
+		$response = new StdClass();
+		//kas selline interest olemas
+		$stmt = $this->connection->prepare("SELECT id FROM Interests WHERE name = ?");
+        echo $this->connection->error;
+		$stmt->bind_param("s", $name);
+        echo $stmt->error;
+		$stmt->execute();
+		
+		//kas oli 1 rida andmeid
+		if($stmt->fetch()){
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "Huviala '".$name."' on juba olemas!";
+			$response->error = $error;
+			return $response;
+		}
+	
+		//*************************
+		//******* OLULINE *********
+		//*************************
+		//panen eelmise käsu kinni
+		$stmt->close();
+	
+		$stmt = $this->connection->prepare("INSERT INTO Interests (name) VALUES (?)");
+		$stmt->bind_param("s", $name);
+		
+		if($stmt->execute()){
+			// edukalt salvestas
+			$success = new StdClass();
+			$success->message = "Huviala edukalt salvestatud";
+			$response->success = $success;
+			
+		}else{
+			// midagi läks katki
+			$error = new StdClass();
+			
+			$error->id =1;
+			echo $stmt->error;
+			$error->message = "Midagi läks katki!";
+			$response->error = $error;
+		}
+		
+		$stmt->close();
+		
+		return $response;
+    }
+    
+    function createDropdown(){
+        
+        $html = '';
+        
+        //liidan eelmisele juurde
+        $html .= '<select name="dropdown_interest">';
+        
+        $stmt = $this->connection->prepare("SELECT id, name FROM Interests");
+        $stmt->bind_result($id, $name);
+        $stmt->execute();
+        
+        //iga rea kohta
+        while($stmt->fetch()){
+            
+            $html .= '<option value="'.$id.'">'.$name.'</option>';
+            
+        }
+        
+        $stmt->close();
+        
+        //$html .= '<option selected >Test 2</option>';
+        
+        $html .= '</select>';
+        
+        return $html;
+        
+    }
+  
+} ?>
--- data.php
+++ data.php
@@ -1,148 +1,72 @@
 <?php
-    require_once("functions.php");
-    
-    if(!isset($_SESSION['user_id'])){
-        header("Location: login.php");
-        // ära enne suunamist midagi rohkem tee
+	require_once("functions.php");
+	require_once("InterestManager.class.php");
+	
+	if(!isset($_SESSION["user_id"])){
+		header("Location: login.php");
         exit();
-    }
-    
-    if(isset($_GET["logout"])){
-        session_destroy();
-        header("Location: login.php");
-    }
-    
-    // profiilipildi üleslaadimine
-    // http://www.w3schools.com/php/php_file_upload.asp
-    
-    $target_dir = "profile_pics/";
-    // faili nimi oleks kasutaja id .jpg
-    $target_file = $target_dir.$_SESSION['user_id'].".jpg";
-    
-    // KAS kasutajal on pilt olemas
-    if(file_exists($target_file)){
-        
-        $profile_image_url = $target_file;
-    }
-    
-    $uploadOk = 1;
-    
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
-            $uploadOk = 0;
-        }
-        // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 1024000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
-        }
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                
-                header("Location: data.php");
-                exit();
-                
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
-        }
-    }
-    
-    
-    if(isset($_GET["delete_image"])){
-        // kustutan pildi/faili
-        unlink($profile_image_url);
-        header("Location: data.php");
+	}
+	
+	if(isset($_GET["logout"])){
+		session_destroy();
+		
+		header("Location: login.php");
         exit();
-        
+	}
+    
+    //***************
+    //** HALDUS *****
+    //***************
+	
+    $InterestManager = new InterestManager($mysqli, $_SESSION["user_id"]);
+    
+    if(isset($_GET["new_interest"])){
+        $add_interest_response = $InterestManager->addInterest($_GET["new_interest"]);
     }
-    
-    
-    
 ?>
-    
 
-Tere, <?=$_SESSION['user_email'];?> <a href="?logout=1">Logi välja</a>
+  <?php if(isset($add_interest_response->success)): ?>
+  
+  <p style="color:green;">
+    <?=$add_interest_response->success->message;?>
+  </p>
+  
+  <?php elseif(isset($add_interest_response->error)): ?>
+  
+  <p style="color:red;">
+    <?=$add_interest_response->error->message;?>
+  </p>
+   
+  <?php endif; ?>
 
-<br>
 
-<?php if(isset($_SESSION['login_message'])): ?>
 
-<p style="color:green;">
-    <?=$_SESSION['login_message'];?>
+<p>
+	Tere, <?=$_SESSION["user_email"];?>
+	<a href="?logout=1"> Logi välja</a>
 </p>
 
-<?php 
-    // kustutan muutuja, et rohkem ei näidataks
-    // ainult 1 korra pärast sisselogimist
-    unset($_SESSION['login_message']);
-endif; ?>
-
-<br>
-
-<h3> Profiilipilt </h3>
-
-<?php if(isset($profile_image_url)): ?>
-
-    <div style="width:200px; height:200px; background-image: url(<?=$profile_image_url;?>); background-size: cover; background-position-x: center; background-position-y: center;" ></div>
-    <br><a href="?delete_image=1">Delete image</a>
-    
-<?php else: ?>
-
-
-<form action="data.php" method="post" enctype="multipart/form-data">
-    Vali profiilipilt:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+</h2>Lisa huviala</h2>
+<form> 
+    <input name="new_interest"> <br>
+    <input type="submit">
 </form>
 
-<?php endif; ?>
-<br><br><br>
-<h2>Galerii</h2>
+<h2>Minu huvialad</h2>
 
-<?php 
+<?=$InterestManager->createDropdown();?>
+<input type="submit" name="lisa" value="lisa">
 
-	//saada k2tte k6ik failid mis on kaustas
-	$images = scandir("profile_pics/");
-	var_dump($images);
-	
-	// iga pildi kohta tekitame lingi
-	
-	for($i = 0; $i < count($images); $i++){
-		//pilt ei ole pilt ega kaks punkti
-		
-		if($images[$i] != "." && $images[$i] != ".."){
-		
-		$link = "profile_pics/".$images[$i];
-		$name = $images[$i]; 
-		
-		echo '<a href="'.$link.'">'.$name.'</a><br>';
-		
-	}
-	}
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
